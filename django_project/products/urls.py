aa87c00868c466485562b335a8edb0fea05f7fe2
Ashish <ash8690@gmail.com>
2018-11-30 13:50:49 +0530

    Update urls.py

--- django_project/products/urls.py
+++ django_project/products/urls.py
@@ -4,11 +4,13 @@ from . import views
 
 urlpatterns = [
         
-            url(r'/sub_categories/for_category=([A-Za-z]+)/$', views.get_sub_categories),          
+            url('/sub_categories', views.get_all_subcategories),
+			url(r'/sub_categories/for_category=([A-Za-z]+)/$', views.get_sub_categories),          
             url(r'/for_category/([A-Za-z]+)/$', views.product_for_category),          
             url(r'/for_sub_category/([A-Za-z]+)/$', views.product_for_sub_category),          
 			url(r'/add/([A-Za-z]+)/([A-Za-z]+)/([A-Za-z]+)/$', views.add_product),          
-            url('/categories', views.get_categories), 
+            url('/categories', views.get_categories),
+				
             url('/get_products', views.get_products), 			
             url('/list', views.index, name='index'),
 			url('/viewTable', views.table, name='table'),
